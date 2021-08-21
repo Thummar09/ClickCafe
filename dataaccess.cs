b853283f1eb04fa48fe048648691a82bf30f3654
krishna <krishna@DESKTOP-QB7V075>
2021-08-21 10:38:17 +0530

    commit

---
+++ dataaccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClickCafe
+{
+    public static class dataaccess
+    {
+        private static string mycon;
+
+        static dataaccess()
+        {
+            mycon = WebConfigurationManager.ConnectionStrings["ClickCafeConnectioString"].ConnectionString;
+        }
+
+        public static DataTable selectQuery(string query)
+
+        {
+            DataTable dt = new DataTable();
+            SqlConnection cnn = new SqlConnection(mycon);
+            cnn.Open();
+            SqlCommand cmd = new SqlCommand(query,cnn);
+            dt.Load(cmd.ExecuteReader());
+            cnn.Close();
+            return dt;
+        }
+    }
+    
+       
+        
+
+        
+    
+
+    
+    
+
+    
+}
